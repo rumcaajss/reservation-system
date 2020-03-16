efe8e1ec591b8c0382c775ee71fffbc025f251db
Andrzej <krawczyk.andrzej93@gmail.com>
2020-03-16 11:00:20 +0100

    Added Debug Statements

--- firestore.js
+++ firestore.js
@@ -1,13 +1,15 @@
 const admin = require("firebase-admin");
+var key = process.env.FIREBASE_PRIVATE_KEY;
+
+console.log('key ' + key);
 
 var app = admin.initializeApp({
   credential: admin.credential.cert({
     "projectId": process.env.FIREBASE_PROJECT_ID,
-    "privateKey": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "privateKey": key.replace(/\\n/g, '\n'),
     "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
   }),
   databaseURL: process.env.FIREBASE_DATABASE_URL,
-
 });
 
 
